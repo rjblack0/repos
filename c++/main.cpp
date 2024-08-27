29c275087fa584c29761963b0b8b9bf21c84f1b1
rjblack0 <rjblack0@live.com>
2024-08-26 21:29:11 -0500

    beginning steps for learning c++ independent

---
+++ c++/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>  // Preprocessor directive that includes the standard input/output stream
+
+int main() { // This initiates the MAIN function which is always named that.
+    std::cout << "Hello, World!" << std::endl;  // Output "Hello, World!" to the console
+    return 0;  // There is no "check" in this mini program, so the function will not know to quit without this line.
+}
+
