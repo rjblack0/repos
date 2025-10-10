0a0c967188dc5b62a6c7b4cfadf7dd8b376d0d7e
rjblack0@ <rjblack0@live.com>
2025-10-10 17:19:48 -0500

    cpp practice

---
+++ prac/cpp/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>      // lets us use cout to print things
+#include <vector>        // gives us access to dynamic lists (vectors)
+#include <algorithm>     // lets us use built-in tools like "sort"
+#include <unordered_set> // gives us a hash set (to track what we've seen)
+
+using namespace std;     // so we can just write "cout" instead of "std::cout"
+
+int main() {
+    std::cout << "Hello, World!";
+    return 0;
+}
+
+pair<int,int> twoSum(vector<int> nums, int target) {
+
+}
